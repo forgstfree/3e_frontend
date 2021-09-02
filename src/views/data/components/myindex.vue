3056df1a926372979c92f2a1586104681faecfb4
docker_3e <gstxhplmm@163.com>
2021-09-02 17:05:18 +0800

    阶段性完成

--- src/views/data/components/myindex.vue
+++ src/views/data/components/myindex.vue
@@ -3,7 +3,7 @@
     <el-upload
       ref="xlsupload"
       class="upload-demo"
-      action="http://localhost:8080/file/structured/"
+      :action="uploadFileUrl"
       drag
       accept=".csv, .xls, .xlsx"
       :data="fileform"
@@ -16,16 +16,11 @@
       <i class="el-icon-upload" />
       <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
       <!-- <el-button slot="trigger" size="small" type="primary">选取文件</el-button> -->
-      <div slot="tip" class="el-upload__tip">
-        只能上传结构化文件，且不超过xkb
-      </div>
+      <div slot="tip" class="el-upload__tip">只能上传结构化文件，且不超过xkb</div>
     </el-upload>
-    <el-button
-      style="margin-left: 10px"
-      size="small"
-      type="success"
-      @click="submitUpload"
-    >上传到服务器</el-button>
+    <el-button style="margin-left: 10px" size="small" type="success" @click="submitUpload"
+      >上传到服务器</el-button
+    >
   </div>
 </template>
 <script>
@@ -33,31 +28,31 @@ export default {
   data() {
     return {
       fileList: [],
+      uploadFileUrl: process.env.VUE_APP_BASE_URL + "/file/structured/",
       fileform: {
-        filename: '',
-        uploader: 'admin'
-      }
-
-    }
+        filename: "",
+        uploader: "admin",
+      },
+    };
   },
   methods: {
     beforeUpload(file) {
-      console.log(file)
-      this.fileform.filename = file.name
+      console.log(file);
+      this.fileform.filename = file.name;
     },
     submitUpload() {
-      this.$refs.xlsupload.submit()
+      this.$refs.xlsupload.submit();
     },
     handleRemove(file, fileList) {
-      console.log(file, fileList)
+      console.log(file, fileList);
     },
     handlePreview(file) {
-      console.log(file)
-    }
-  }
-}
+      console.log(file);
+    },
+  },
+};
 </script>
-<style >
+<style>
 .my-upload-container {
   text-align: center; /*让div内部文字居中*/
 }
